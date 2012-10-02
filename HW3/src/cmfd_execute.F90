46e474976578f412dd2d525744d2c0b5e83a8a23
Bryan Herman <bherman@mit.edu>
2012-10-02 17:46:07 -0400

    put logical around kinetics run

--- HW3/src/cmfd_execute.F90
+++ HW3/src/cmfd_execute.F90
@@ -17,7 +17,7 @@ contains
 !---external references
 
     use error,       only: fatal_error
-    use global,      only: solver_type, message
+    use global,      only: solver_type, message, run_kinetics
     use kinetics_solver,  only: kinetics_execute
     use math,        only: csr_jacobi, csr_gauss_seidel
     use output,      only: header
@@ -42,7 +42,7 @@ contains
     end select
 
     ! call kinetics
-    call kinetics_execute(csr_gauss_seidel)
+    if (run_kinetics) call kinetics_execute(csr_gauss_seidel)
 
   end subroutine execute_cmfd
 
